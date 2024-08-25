80ea9c2e8143fd7077047901afb920a310534dcb
Vlad Pechkin <pechkinde@gmail.com>
2024-08-25 13:41:31 +0400

    Fix deploy

--- app/[lang]/components/page.tsx
+++ app/[lang]/components/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { LandingLayout } from "@/equix/Landing/LandingLayout";
-import { Box, Card } from "@/equix/components/Box";
+import { Box } from "@/equix/components/Box";
 import { Row } from "@/equix/components/Flex";
 import { H2 } from "@/equix/components/Heading";
 import componentsData from "./data";
@@ -15,7 +15,11 @@ const Page = () => (
     </p>
     <Row className="flex-wrap w-full">
       {Object.keys(componentsData).map((componentName, index) => (
-        <Box key={index} className="border border-inherit" href={`/components/${componentName}`}>
+        <Box
+          key={index}
+          className="border border-inherit"
+          href={`/components/${componentName}`}
+        >
           {componentName}
         </Box>
       ))}
